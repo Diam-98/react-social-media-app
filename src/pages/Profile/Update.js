8525b303ba78c395bd1c129fbec4f9c6f21da18e
Diam Diallo <diamdiallo98@gmail.com>
2023-12-17 22:49:16 +0000

    profle setup

--- src/pages/Profile/Update.js
+++ src/pages/Profile/Update.js
@@ -1,13 +1,46 @@
 import { EditOutlined } from '@ant-design/icons';
 import React from 'react';
+import profile from '../../images/robot-avatar.png';
 
 const Update = () => {
   return (
     <div className='profile'>
-      <span>
-        <EditOutlined />
-        <h2>Modifier Profile</h2>
-      </span>
+      <h2>Modifier Profile</h2>
+      <div className='profile-form'>
+        <form>
+          <div className='existing-image'>
+            <img src={profile} alt='profile' />
+            <input type='file' />
+          </div>
+          <div className='form-input'>
+            <label htmlFor='fistName'>Prenom</label>
+            <input
+              type='text'
+              id='firstName'
+              name='firstName'
+              placeholder='Prenom'
+            />
+          </div>
+          <div className='form-input'>
+            <label htmlFor='lastName'>Nom</label>
+            <input
+              type='text'
+              id='lastName'
+              name='lastName'
+              placeholder='Nom'
+            />
+          </div>
+          <div className='form-input'>
+            <label htmlFor='email'>Email</label>
+            <input type='email' name='email' id='email' placeholder='Email' />
+          </div>
+          <div className='form-input'>
+            <label htmlFor='bio'>Bio</label>
+            <textarea name='bio' id='bio' placeholder='Biographie'></textarea>
+          </div>
+          <button htmlType='submit'>Modifier</button>
+        </form>
+      </div>
     </div>
   );
 };
