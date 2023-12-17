0895e343e125cf55b95d814f81ccfe74d3930e88
Diam Diallo <diamdiallo98@gmail.com>
2023-12-17 11:11:44 +0000

    home page responsive

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './sidebar.css';
 import { Link } from 'react-router-dom';
 import {
@@ -10,37 +10,53 @@ import {
 } from '@ant-design/icons';
 
 const Sidebar = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    updateDimensions();
+
+    window.addEventListener('resize', updateDimensions);
+
+    return () => {
+      window.removeEventListener('resize', updateDimensions);
+    };
+  }, []);
+
+  const updateDimensions = () => {
+    setIsMobile(window.innerWidth <= 650);
+  };
+
   return (
-    <div className='sidebar'>
+    <div className={isMobile ? 'bottom-bar' : 'sidebar'}>
       <ul>
         <li className='menu-item active'>
           <Link to='/' className='nav-link'>
             <HomeOutlined />
-            <span>Accueil</span>
+            <span className='link-text'>Accueil</span>
           </Link>
         </li>
         <li className='menu-item'>
           <Link to='/' className='nav-link'>
             <SwitcherOutlined />
-            <span>Explorer</span>
+            <span className='link-text'>Explorer</span>
           </Link>
         </li>
         <li className='menu-item'>
           <Link to='/' className='nav-link'>
             <TeamOutlined />
-            <span>Reseaux</span>
+            <span className='link-text'>Reseaux</span>
           </Link>
         </li>
         <li className='menu-item'>
           <Link to='/' className='nav-link'>
             <SaveOutlined />
-            <span>Enregistre</span>
+            <span className='link-text'>Enregistre</span>
           </Link>
         </li>
         <li className='menu-item'>
           <Link to='/' className='nav-link'>
             <FormOutlined />
-            <span>Publier</span>
+            <span className='link-text'>Publier</span>
           </Link>
         </li>
       </ul>
